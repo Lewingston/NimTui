c9259787fbd530cab3fda6597f31344497f7496e
Lewingston <jonas.speyer@web.de>
2026-02-01 23:44:31 +0100

    Added more colors

--- src/Common/Color.cpp
+++ src/Common/Color.cpp
@@ -3,8 +3,10 @@
 
 using namespace TUI;
 
-const Color Color::WHITE { 255, 255, 255 };
-const Color Color::BLACK {   0,   0,   0 };
-const Color Color::RED   { 255,   0,   0 };
-const Color Color::GREEN {   0, 255,   0 };
-const Color Color::BLUE  {   0,   0, 255 };
+const Color Color::WHITE  { 255, 255, 255 };
+const Color Color::BLACK  {   0,   0,   0 };
+const Color Color::RED    { 255,   0,   0 };
+const Color Color::GREEN  {   0, 255,   0 };
+const Color Color::BLUE   {   0,   0, 255 };
+const Color Color::YELLOW { 255, 255,   0 };
+const Color Color::ORANGE { 255, 128,   0 };
--- src/Common/Color.h
+++ src/Common/Color.h
@@ -13,6 +13,8 @@ namespace TUI {
             static const Color RED;
             static const Color BLUE;
             static const Color GREEN;
+            static const Color YELLOW;
+            static const Color ORANGE;
 
             constexpr Color(u8 r, u8 g, u8 b) :
                 r(r), g(g), b(b) {}
