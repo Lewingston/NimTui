71eafb0cdc5ade7166b1423b875e97f435a160c1
Lewingston <jonas.speyer@web.de>
2026-02-02 02:40:06 +0100

    Completed implementation of Utf8Char class and tests.

--- src/Common/Utf8Char.h
+++ src/Common/Utf8Char.h
@@ -6,28 +6,32 @@
 #include <string>
 #include <limits>
 
-namespace ImTui {
+namespace TUI {
 
     class Utf8Char final {
 
         public:
 
-            constexpr Utf8Char(const std::string& str) {
+            constexpr Utf8Char() = default;
 
-                if (str.empty())
+
+            constexpr Utf8Char(const char* str) {
+
+                const u8 count = getByteCount(static_cast<u8>(str[0]));
+                if (count > 4)
                     return;
 
-                const u8 count = getByteCount(static_cast<u8>(str.at(0)));
-
-                for (u8 ii = 0; ii < count && ii < str.size(); ii++) {
-                    bytes[ii] = static_cast<u8>(str.at(ii));
+                for (u8 ii = 0; ii < count; ii++) {
+                    bytes[ii] = static_cast<u8>(str[ii]);
                 }
-            }
+            };
 
 
             [[nodiscard]] static constexpr u8 getByteCount(const u8 firstByte) noexcept {
 
-                if ((firstByte & 0b1000'0000) == 0)
+                if (firstByte == 0)
+                    return 0;
+                else if ((firstByte & 0b1000'0000) == 0)
                     return 1;
                 else if ((firstByte & 0b1110'0000) == 0b1100'0000)
                     return 2;
@@ -54,15 +58,23 @@ namespace ImTui {
 
             [[nodiscard]] constexpr std::string toString() const {
 
-                const u8 size = getByteCount();
-                if (size > 4)
-                    return "";
+                return std::string(reinterpret_cast<const char*>(bytes.data()), getByteCount());
+            }
 
-                return std::string(reinterpret_cast<const char*>(bytes.data()), size);
+
+            [[nodiscard]] constexpr bool operator==(const Utf8Char& character) const noexcept {
+
+                return bytes == character.bytes;
             }
 
         private:
 
             std::array<u8, 4> bytes { 0 };
     };
+
+
+    [[nodiscard]] inline constexpr Utf8Char operator ""_u8(const char* str, std::size_t) {
+
+        return Utf8Char(str);
+    }
 }
--- test/Common/Utf8Char.test.cpp
+++ test/Common/Utf8Char.test.cpp
@@ -3,13 +3,16 @@
 
 #include "Common/Utf8Char.h"
 
-using namespace ImTui;
+using namespace TUI;
 
 
 TEST(Utf8Char, GetByteCount) {
 
+    { // zero byte
+        EXPECT_EQ(Utf8Char::getByteCount(0), 0);
+    }
+
     { // one byte
-        EXPECT_EQ(Utf8Char::getByteCount(0), 1);
         EXPECT_EQ(Utf8Char::getByteCount(0b0010'1111), 1);
     }
 
@@ -31,15 +34,16 @@ TEST(Utf8Char, GetByteCount) {
     { // invalid
         EXPECT_GT(Utf8Char::getByteCount(0b1000'0000), 4);
         EXPECT_GT(Utf8Char::getByteCount(0b1111'1000), 4);
+        EXPECT_GT(Utf8Char::getByteCount(0b1111'1111), 4);
     }
 }
 
 
-TEST(Utf8Char, ConstructFromString) {
+TEST(Utf8Char, ConstructFromCharPtr) {
 
     { // Test ASCII character
 
-        Utf8Char utf8Char("a");
+        constexpr Utf8Char utf8Char("a");
 
         // Check number of utf8 bytes
         EXPECT_EQ(utf8Char.getByteCount(), 1);
@@ -50,15 +54,13 @@ TEST(Utf8Char, ConstructFromString) {
         EXPECT_EQ(utf8Char.getBytes().at(2), 0);
         EXPECT_EQ(utf8Char.getBytes().at(3), 0);
 
-        // Check conversion back to string
+        // Check conversion to string
         EXPECT_EQ(utf8Char.toString(), "a");
-
-        std::cout << "LOL: " << utf8Char.toString() << '\n';
     }
 
     { // Test Umlaut
 
-        Utf8Char utf8Char("ü");
+        constexpr Utf8Char utf8Char("ü");
 
         // Check number of utf8 bytes
         EXPECT_EQ(utf8Char.getByteCount(), 2);
@@ -69,15 +71,13 @@ TEST(Utf8Char, ConstructFromString) {
         EXPECT_EQ(utf8Char.getBytes().at(2), 0x00);
         EXPECT_EQ(utf8Char.getBytes().at(3), 0x00);
 
-        // Check conversion back to string
+        // Check conversion to string
         EXPECT_EQ(utf8Char.toString(), "ü");
-
-        std::cout << "LEL: " <<  utf8Char.toString() << '\n';
     }
 
     { // Test emoji
 
-        Utf8Char utf8Char("😉");
+        constexpr Utf8Char utf8Char("😉");
 
         // Check number of utf8 bytes
         EXPECT_EQ(utf8Char.getByteCount(), 4);
@@ -88,7 +88,61 @@ TEST(Utf8Char, ConstructFromString) {
         EXPECT_EQ(utf8Char.getBytes().at(2), 0x98);
         EXPECT_EQ(utf8Char.getBytes().at(3), 0x89);
 
-        // Check conversion back to string
+        // Check conversion to string
         EXPECT_EQ(utf8Char.toString(), "😉");
+    }
+
+    { // Test empty string
+
+        constexpr Utf8Char utf8Char("");
+
+        // Check number of utf8 bytes
+        EXPECT_EQ(utf8Char.getByteCount(), 0);
+
+        // Check every single byte
+        EXPECT_EQ(utf8Char.getBytes().at(0), 0);
+        EXPECT_EQ(utf8Char.getBytes().at(1), 0);
+        EXPECT_EQ(utf8Char.getBytes().at(2), 0);
+        EXPECT_EQ(utf8Char.getBytes().at(3), 0);
+
+        // Check conversion to string
+        EXPECT_EQ(utf8Char.toString(), "");
+    }
+
+    { // Test construction from invalid utf8 string
+
+        // Do not use constexpr for better code coverage results!
+        const char str[] = { static_cast<char>(0b1111'1111), 0, 0, 0 };
+        const Utf8Char utf8Char(str);
+
+        // Check number of bytes
+        EXPECT_EQ(utf8Char.getByteCount(), 0);
+
+        // Check every single byte
+        EXPECT_EQ(utf8Char.getBytes().at(0), 0);
+        EXPECT_EQ(utf8Char.getBytes().at(1), 0);
+        EXPECT_EQ(utf8Char.getBytes().at(2), 0);
+        EXPECT_EQ(utf8Char.getBytes().at(3), 0);
+
+        // Check conversion to string
+        EXPECT_EQ(utf8Char.toString(), "");
+    }
+}
+
+
+TEST(Utf8Char, EqualsOperator) {
+
+    { // Should be equal
+        Utf8Char char1("Ä");
+        Utf8Char char2("Ä");
+
+        EXPECT_TRUE(char1 == char2);
+    }
+
+    { // Should not be equal
+        Utf8Char char1("Ä");
+        Utf8Char char2("Ü");
+
+        EXPECT_FALSE(char1 == char2);
     }
 }
