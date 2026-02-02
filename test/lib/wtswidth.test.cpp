b1d3b0dd6f11b2cd6e3eb0d71ee8df6e2ae585c8
Lewingston <jonas.speyer@web.de>
2026-02-02 18:01:33 +0100

    Added library to check if character is normal width or full width

---
+++ test/lib/wtswidth.test.cpp
@@ -0,0 +1,29 @@
+
+#include <gtest/gtest.h>
+
+#include "wtswidth.h"
+
+
+TEST(Lib, wtswidth) {
+
+    { // ascii character normal width
+
+        const std::string str = "A";
+
+        EXPECT_EQ(wts8width(str.c_str(), str.length()), 1);
+    }
+
+    { // umlaut character noraml width
+
+        const std::string str = "ü";
+
+        EXPECT_EQ(wts8width(str.c_str(), str.length()), 1);
+    }
+
+    { // emoji character full width
+
+        const std::string str = "😡";
+
+        EXPECT_EQ(wts8width(str.c_str(), str.length()), 2);
+    }
+}
