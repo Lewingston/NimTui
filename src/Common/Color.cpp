05abb59fca166f91f65c910f8bcce8c8830e2b09
Lewingston <jonas.speyer@web.de>
2026-03-01 14:53:16 +0100

    Added alpha channel

--- src/Common/Color.cpp
+++ src/Common/Color.cpp
@@ -3,6 +3,8 @@
 
 using namespace TUI;
 
+const Color Color::TRANSP { 0, 0, 0, 0 };
+
 const Color Color::WHITE  { 255, 255, 255 };
 const Color Color::BLACK  {   0,   0,   0 };
 const Color Color::RED    { 255,   0,   0 };
@@ -12,3 +14,31 @@ const Color Color::YELLOW { 255, 255,   0 };
 const Color Color::ORANGE { 255, 128,   0 };
 const Color Color::PURPLE { 128,   0, 128 };
 const Color Color::GRAY   { 128, 128, 128 };
+
+
+Color Color::overlay(Color color) const noexcept {
+
+    return color.multAlpha(color.a).add(this->multAlpha((255 - color.a)));
+}
+
+
+Color Color::multAlpha(u8 alpha) const noexcept {
+
+    const f32 fa = static_cast<f32>(alpha) / 255.0f;
+
+    return Color(
+        static_cast<u8>(static_cast<f32>(r) * fa),
+        static_cast<u8>(static_cast<f32>(g) * fa),
+        static_cast<u8>(static_cast<f32>(b) * fa)
+    );
+}
+
+
+Color Color::add(Color color) const noexcept {
+
+    return Color(
+        r + color.r,
+        g + color.g,
+        b + color.b
+    );
+}
--- src/Common/Color.h
+++ src/Common/Color.h
@@ -4,10 +4,11 @@
 
 namespace TUI {
 
-    class Color {
+    class Color final {
 
         public:
 
+            static const Color TRANSP;
             static const Color WHITE;
             static const Color BLACK;
             static const Color RED;
@@ -18,22 +19,29 @@ namespace TUI {
             static const Color PURPLE;
             static const Color GRAY;
 
-            constexpr Color(u8 r, u8 g, u8 b) :
-                r(r), g(g), b(b) {}
+            constexpr Color(u8 r, u8 g, u8 b, u8 a = 255) :
+                r(r), g(g), b(b), a(a) {}
 
             [[nodiscard]] constexpr u8 getR() const noexcept { return r; }
             [[nodiscard]] constexpr u8 getG() const noexcept { return g; }
             [[nodiscard]] constexpr u8 getB() const noexcept { return b; }
+            [[nodiscard]] constexpr u8 getA() const noexcept { return a; }
 
             [[nodiscard]] constexpr bool operator==(const Color& c) const noexcept {
-                return r == c.r && g == c.g && b == c.b;
+                return r == c.r && g == c.g && b == c.b && a == c.a;
             }
 
+            [[nodiscard]] Color overlay(Color color) const noexcept;
+
         private:
+
+            [[nodiscard]] Color multAlpha(u8 alpha) const noexcept;
+            [[nodiscard]] Color add(Color color) const noexcept;
 
             u8 r = 0;
             u8 g = 0;
             u8 b = 0;
+            u8 a = 0;
 
     };
 }
