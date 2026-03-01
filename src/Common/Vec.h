9fd010775263f4ffedb4a504decc5ad154191759
Lewingston <jonas.speyer@web.de>
2026-03-01 11:54:08 +0100

    Implemented implicit conversion, increment operator and multiplication.

--- src/Common/Vec.h
+++ src/Common/Vec.h
@@ -10,6 +10,12 @@ namespace TUI {
             Vec2() = default;
             Vec2(T x, T y) : x(x), y(y) {}
 
+            // Conversion constructor
+            template<typename U>
+            Vec2(const Vec2<U>& v) :
+                x(static_cast<T>(v.getX())),
+                y(static_cast<T>(v.getY())) {}
+
             [[nodiscard]] T getX() const noexcept { return x; }
             [[nodiscard]] T getY() const noexcept { return y; }
 
@@ -17,6 +23,14 @@ namespace TUI {
             [[nodiscard]] T getHeight() const noexcept { return y; }
 
             [[nodiscard]] T getArea() const noexcept { return x * y; }
+
+            Vec2<T>& operator+=(const Vec2<T>& v) {
+
+                x += v.x;
+                y += v.y;
+
+                return *this;
+            }
 
         private:
 
@@ -35,5 +49,11 @@ namespace TUI {
     [[nodiscard]] Vec2<T> operator-(const Vec2<T>& vec1, const Vec2<T>& vec2) {
 
         return Vec2<T>(vec1.getX() - vec2.getX(), vec1.getY() - vec2.getY());
+    }
+
+    template<typename T, typename U>
+    [[nodiscard]] Vec2<T> operator*(const Vec2<T>& vec, U mult) {
+
+        return Vec2<T>(vec.getX() * mult, vec.getY() * mult);
     }
 }
