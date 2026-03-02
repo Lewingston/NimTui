f6ce3da23217f13f61e7c5b0ae38f2c227441fb4
Lewingston <jonas.speyer@web.de>
2026-03-02 19:32:37 +0100

    Make use of primitive rectangle for drawing.

--- src/Widgets/Widget.cpp
+++ src/Widgets/Widget.cpp
@@ -1,5 +1,6 @@
 
 #include "Widget.h"
+#include "Primitives/Rect.h"
 #include "Console/RenderBuffer.h"
 
 using namespace TUI;
@@ -27,21 +28,10 @@ void Widget::setPos(Vec2<s32> pos) {
 
 void Widget::draw(RenderBuffer& buffer, Vec2<s32> offset) {
 
-    for (s32 y = 0; y < static_cast<s32>(size.getHeight()); y++) {
-        for (s32 x = 0; x < static_cast<s32>(size.getWidth()); x++) {
+    Rect rect(offset + getPos(), getSize());
+    rect.setBackColor(backColor);
 
-            const s32 posX = pos.getX() + x + offset.getX();
-            const s32 posY = pos.getY() + y + offset.getY();
-
-            const Vec2<u32> pos = { static_cast<u32>(posX), static_cast<u32>(posY) };
-
-            buffer.set(pos, {
-                " ",
-                buffer.get(pos).frontColor,
-                backColor
-            });
-        }
-    }
+    rect.draw(buffer);
 }
 
 
